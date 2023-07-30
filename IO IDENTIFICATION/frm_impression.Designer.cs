dc126e5dc1163915638d3d8a92293938bec5472c
Landry Mutunda <mutundalandry@gmail.com>
2023-07-30 21:00:24 +0200

    debut

--- IO IDENTIFICATION/frm_impression.Designer.cs
+++ IO IDENTIFICATION/frm_impression.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 466);
+            this.ClientSize = new System.Drawing.Size(458, 466);
             this.Name = "frm_impression";
             this.Text = "Impression";
             this.ResumeLayout(false);
